acba01c91edd1310cde0af666dde7c71c70868a5
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-22 21:09:54 +0700

    Add implementation of grid using flexbox only

--- etchasketch.js
+++ etchasketch.js
@@ -1,13 +1,16 @@
 // Create Square Divs to attach to a parent div
-function createSquareDivs(parentDivID, squareDivName, num) {
+function createRows(parentDivID, numOfSquares) {
     const parent = document.getElementById(parentDivID);
+    const divSize = (500 / numOfSquares);
+    const totalSquares = numOfSquares * numOfSquares;
     
-    for(let i = 0; i <= num; i++) {
+    for(let i = 0; i < totalSquares; i++) {
         const squareDiv = document.createElement("div");
         squareDiv.classList.add("squareDiv");
+        squareDiv.style.width = `${divSize}px`;
+        squareDiv.style.height = `${divSize}px`;
         parent.appendChild(squareDiv);
     }
 }
 
-createSquareDivs("container", 16);
-createSquareDivs("container", 16);
+createRows("container", 16)
