a72959dba4d57d4af6f392708adf96368f527205
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-22 22:17:17 +0700

    Add randomRGB and solidifying opacity implementation

--- etchasketch.js
+++ etchasketch.js
@@ -7,13 +7,35 @@ function createGrid(parentDivID, numOfSquares) {
     for(let i = 0; i < totalSquares; i++) {
         const squareDiv = document.createElement("div");
         squareDiv.classList.add("squareDiv");
+        squareDiv.style.opacity = "0.1";
         squareDiv.addEventListener('mouseover', function(e) {
-            e.target.style.backgroundColor = `#000`;
+            e.target.style.backgroundColor = `rgb(${randomRGB()})`;
+            increaseOpacity(e.target);
         })
         squareDiv.style.width = `${divSize}px`;
         squareDiv.style.height = `${divSize}px`;
         parent.appendChild(squareDiv);
     }
+}
+
+// Generate random rgb value
+function randomRGB () {
+    const r = randomInteger(255);
+    const g = randomInteger(255);
+    const b = randomInteger(255);
+
+    return r + "," + g + "," + b;
+}
+
+// Generate random integer from 0 to max
+function randomInteger(maxNum) {
+    return Math.floor(Math.random() * maxNum);
+}
+
+// Increase opacity given an element
+function increaseOpacity(ele) {
+    let curOpac = getComputedStyle(ele).opacity;
+    ele.style.opacity = parseFloat(curOpac) + 0.1;
 }
 
 const gridBtn = document.getElementById("gridBtn");
@@ -29,11 +51,10 @@ gridBtn.addEventListener("click", function (e) {
     }
 
     // Draw the grid
-    if(desiredGridSize < 100 && desiredGridSize >= 0)
+    if(desiredGridSize <= 100 && desiredGridSize >= 0)
     {
         createGrid("container", desiredGridSize);
     } else {
         alert("Invalid grid size!");
     }
 });
-
